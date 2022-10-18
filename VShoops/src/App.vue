2e8d7b30b63b478002ab91776bc2a8d37ba7db4e
liuyinghao1220 <1219829810@qq.com>
2022-10-18 21:36:22 +0800

    123

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
+  <!-- <div>
     <el-radio-group  size="large">
       <el-radio-button label="New York" />
       <el-radio-button label="Washington" />
       <el-radio-button label="Los Angeles" />
       <el-radio-button label="Chicago" />
     </el-radio-group>
-  </div>
+  </div> -->
+  <!-- <VideoDetail /> -->
+  <Video />
 </template>
 
 <script setup lang="ts">
+import Video from './view/home/lyh/video.vue'
 
 </script>
 
--- VShoops/src/main.ts
+++ VShoops/src/main.ts
@@ -5,12 +5,14 @@ import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 
+//引入视频详情全局组件
+import VideoDetail from './components/video/VideoDetail.vue'
 // createApp(App).mount('#app')
 
 
 const app = createApp(App)
 
-
+app.component('VideoDetail',VideoDetail)
 app.use(ElementPlus, {
     locale: zhCn
   }).mount('#app')
