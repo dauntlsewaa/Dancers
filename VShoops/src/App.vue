038a36b68b2b2e33f54a864f18004c6c18bd6b89
邢志策 <812243211@qq.com>
2022-10-18 20:22:43 +0800

    123

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,16 +1,8 @@
 <template>
-  <div>
-    <el-radio-group  size="large">
-      <el-radio-button label="New York" />
-      <el-radio-button label="Washington" />
-      <el-radio-button label="Los Angeles" />
-      <el-radio-button label="Chicago" />
-    </el-radio-group>
-  </div>
+  <Footer></Footer>
 </template>
-
 <script setup lang="ts">
-
+import Footer from './components/footer/footer.vue'
 </script>
 
 <style scoped>
