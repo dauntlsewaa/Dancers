b83f8eb254e8b48f74f1c5929e768074e3e3150b
刘亚静 <lyj638956>
2022-10-19 18:33:10 +0800

    综合和二维码

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <Home />
+    <!-- <Home /> -->
+    <!-- <Synthesize></Synthesize> -->
+    <QR></QR>
   </div>
 </template>
 
 <script setup lang="ts">
+import QR from './components/QR/index.vue';
 import Home from './views/home/index.vue'
+// import Synthesize from './views/class/synthesize/index.vue'
 </script>
 
 <style scoped>
