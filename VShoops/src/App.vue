2f9ec258936efa7eefb86d397ee135c6718909f7
刘亚静 <lyj638956>
2022-10-19 22:53:17 +0800

    我的资料

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,40 +1,18 @@
 <template>
-<<<<<<< HEAD
-  <div id="app">
-     <!-- <training></training> -->
-    <Home />
-
-=======
-  <div>
-    <!-- <Home /> -->
-    <!-- <Synthesize></Synthesize> -->
-    <QR></QR>
->>>>>>> liuyajing
-  </div>
+   <div>
+       <Means></Means>
+   </div>
 </template>
 
 <script setup lang="ts">
-<<<<<<< HEAD
-
-
+import Means from './components/means/index.vue';
+// import Training from './views/training/index.vue'
+// import Home from "./views/home/index.vue"
 // import Training from './views/training/index.vue'
 
 
 
 
-import Home from "./views/home/index.vue"
-
-
-// import Training from './views/training/index.vue'
-
-
-
-
-=======
-import QR from './components/QR/index.vue';
-import Home from './views/home/index.vue'
-// import Synthesize from './views/class/synthesize/index.vue'
->>>>>>> liuyajing
 </script>
 
 <style scoped>
