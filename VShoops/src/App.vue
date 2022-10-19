425cd3b6b2bc5f2123224d1998ed0c29c8521793
阿残 <2445377183@qq.com>
2022-10-19 22:27:00 +0800

    订单详情

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-     <training></training>
+  <div>
     <!-- <Home /> -->
-   <!-- <Nav></Nav> -->
+    <order-detail></order-detail>
   </div>
 </template>
 
 <script setup lang="ts">
+import OrderDetail from './components/orderdetail/OrderDetail.vue';
 // import Home from "./views/home/index.vue"
-import Training from './views/training/index.vue'
-// import Nav from './views/course/nav/index.vue'
+
+
 
 
 
