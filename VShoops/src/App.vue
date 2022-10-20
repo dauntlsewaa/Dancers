83fe1a75933f0943efe4b8ffbbb89563e3868cbe
阿残 <2445377183@qq.com>
2022-10-20 10:13:25 +0800

    支付详情

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+  <orderdetail></orderdetail>
+
   </div>
 </template>
 
 <script setup lang="ts">
+import Orderdetail from "./components/orderdetail/index.vue";
 </script>
 
 <style scoped></style>
