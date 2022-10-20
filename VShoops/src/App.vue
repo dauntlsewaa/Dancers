11168c7c8e96e7430022a36a8b11af9f3fcbb8e9
yzcong <2757885476@qq.com>
2022-10-20 13:58:47 +0800

    杨朝聪-我的会员

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,39 +1,17 @@
 <template>
-<<<<<<< HEAD
-  <div id="app">
-    <Mymember/>
     <!-- <training></training> -->
-    <hr>
-    <hr>
-    <hr>
     <Home />
-
-=======
-  <div>
     <router-view></router-view>
->>>>>>> e2e39d607ffa489fb304ac2a11af6e1c024634a0
-  </div>
 </template>
 
 <script setup lang="ts">
-<<<<<<< HEAD
-
 
 // import Training from './views/training/index.vue'
-
-
-
 
 import Home from "./views/home/index.vue"
-import Mymember from "./components/mymember/mymember.vue"
 
 // import Training from './views/training/index.vue'
 
-
-
-
-=======
->>>>>>> e2e39d607ffa489fb304ac2a11af6e1c024634a0
 </script>
 
 <style scoped></style>
