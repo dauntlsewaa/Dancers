a62d7cac0cebe3b3a718209ffd3d2361e283aaed
刘亚静 <lyj638956>
2022-10-20 09:12:52 +0800

    我的资料

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <means></means>
   </div>
 </template>
 
 <script setup lang="ts">
+import Means from './components/means/index.vue';
 </script>
 
 <style scoped></style>
