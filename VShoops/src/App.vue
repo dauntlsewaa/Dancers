239b07e38f73640cce1a8c2656d03193303eab3e
dauntlsewaa <116052278+dauntlsewaa@users.noreply.github.com>
2022-10-21 15:08:07 +0800

    Update App.vue

--- VShoops/src/App.vue
+++ VShoops/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <router-view></router-view>
-<<<<<<< HEAD
-
-
 
   </div>
 </template>
@@ -16,19 +13,3 @@
   
 
 
- 
-
-
-
-
-
-<script setup lang="ts"></script>
-
-=======
-  </div>
-</template>
-
-<script setup lang="ts"></script>
->>>>>>> 3a83efa0d27484caf1eeefeb8a3f21046b5da0ed
-
-<style></style>
